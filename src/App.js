bd28f8e3afa6c769b602bf53479d1bb1d10691a0
Prabhat Bahukhandi <prabhat.bahukhandi@myntra.com>
2019-05-31 09:14:42 +0530

    npm init => Dev dependency: Prettier, and prettier extension for code formatting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 // Writing Pure React here.
 const Pet = props => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
-}
+};
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement('h1', {}, 'Adopt Me'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'Bird',
-        breed: 'Cockteil'
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'Cat',
-        breed: 'Mixed'
-      })
-    ]
-  );
-}
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockteil"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
