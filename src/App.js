26e5d89718f8bcbc4100ca15841b560775d836f7
Prabhat Bahukhandi <prabhat.bahukhandi@myntra.com>
2019-06-01 12:33:18 +0530

    Reach Router: Adding a scond page and handling routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
+// Router
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+
 const App = () => {
   return (
     <div>
-      <h1> Adopt Me </h1>
-      <SearchParams />
+      <header>
+        <Link to="/"> Adopt Me </Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="details/:id" />
+      </Router>
     </div>
   );
 };
